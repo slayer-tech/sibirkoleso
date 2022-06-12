7305fb4f8130c85e36acfcd10f6553fa5c7ab122
slayer-tech <51415821+slayer-tech@users.noreply.github.com>
2022-06-12 16:07:18 +0300

    Add files via upload

---
+++ js/filter.js
@@ -0,0 +1,381 @@
+setTimeout(() => {
+    let inputName = new Set()
+    let catalogBlock = $('.catalog__block')
+    let selector = $('.filter .row:not(.cars) input[type="radio"]:not(:checked)+label, .filter .row:not(.cars) input[type="checkbox"]+label');
+    func(selector, inputName, catalogBlock, filterType)
+}, 3000)
+
+function func(selector, inputName, catalogBlock, filterType) {
+    let filterTitle = $('.filter__type-title')
+    let filterSeasons = $('.filter__seasons')
+
+    selector.on('click', function () {
+        $('.filter__loading').css('display', 'block')
+        $('.catalog__loading').css('display', 'block')
+
+
+        let inputs
+        if (filterType == 'wheels') {
+            inputs = {
+                "porting": [],
+                "diameter": [],
+                "width": [],
+                "gab": [],
+                "dia": [],
+                "brand": [],
+            }
+        }
+        else {
+            inputs = {
+                "width": [],
+                "height": [],
+                "diameter": [],
+                "brand": [],
+                "season": [],
+                "spike": [],
+                "type": []
+            }
+        }
+
+
+        setTimeout(async () => {
+            catalogBlock.html("")
+            await $('.row.active').find('input:checked').each((i, e) => {
+                let el = $(e)
+                inputs[el.attr("name")].push(el.attr('value'))
+            })
+
+            if (filterType != 'wheels') {
+                inputs.spike[0] = !!inputs.spike.length;
+
+                if (inputs.type.length == 0) {
+                    inputs.type[0] = 1
+                }
+            }
+
+            if (filterType == 'wheels') {
+                $.ajax({
+                    url: '/wheels.json',
+                    success: (data) => {
+                        let porting = new Set()
+                        let diameter = new Set()
+                        let width = new Set()
+                        let gab = new Set()
+                        let dia = new Set()
+                        let brand = new Set()
+                        let empty = true
+                        let match = true
+
+                        for (let input in inputs) {
+                            inputs[input].forEach(() => {
+                                empty = false
+                            })
+                            if (!empty) {
+                                break
+                            }
+                        }
+
+                        if (empty) {
+                            $.ajax({
+                                url: "/porting.json",
+                                success: data => {
+                                    $(".filter__item-list.porting").html("")
+                                    fillFilterItem(data, $('.row.wheels .filter__item-list.porting'), 'porting')
+                                }
+                            })
+                            $.ajax({
+                                url: "/diameters.json",
+                                success: data => {
+                                    $(".filter__item-list.diameter").html("")
+                                    fillFilterItem(data, $('.row.wheels .filter__item-list.diameter'), 'diameter')
+                                }
+                            })
+                            $.ajax({
+                                url: "/width-wheel.json",
+                                success: data => {
+                                    $(".filter__item-list.width").html("")
+                                    fillFilterItem(data.sort(), $('.row.wheels .filter__item-list.width'), 'width')
+                                }
+                            })
+                            $.ajax({
+                                url: "/gab.json",
+                                success: data => {
+                                    $(".filter__item-list.gab").html("")
+                                    fillFilterItem(data, $('.row.wheels .filter__item-list.gab'), 'gab')
+                                }
+                            })
+                            $.ajax({
+                                url: "/dia.json",
+                                success: data => {
+                                    $(".filter__item-list.dia").html("")
+                                    fillFilterItem(data, $('.row.wheels .filter__item-list.dia'), 'dia')
+                                }
+                            })
+                            $.ajax({
+                                url: "/brand-wheel.json",
+                                success: data => {
+                                    $(".filter__item-list.brand").html("")
+                                    fillFilterItem(data, $('.row.wheels .filter__item-list.brand'), 'brand')
+                                }
+                            })
+
+                            $('.filter__loading').css('display', 'none')
+                            data.forEach(d => {
+                                fillCatalog(d)
+                            })
+                            inputName.clear()
+                        }
+                        catalogBlock.html("")
+
+                        data.forEach(d => {
+                            for (let input in inputs) {
+                                if (match) {
+                                    for (let val in inputs[input]) {
+                                        if (d[input] == inputs[input][val]) {
+                                            match = true;
+                                            porting.add(d.porting)
+                                            diameter.add(d.diameter)
+                                            width.add(d.width)
+                                            gab.add(d.gab)
+                                            dia.add(d.dia)
+                                            brand.add(d.brand)
+                                            break;
+                                        }
+                                        match = false
+                                    }
+                                }
+                            }
+                            if (match) {
+                                fillCatalog(d)
+                            }
+                            match = true
+                        })
+
+                        inputName.add($(this).prev().attr("name"))
+
+                        if (!inputName.has('porting')) {
+                            $(".filter__item-list.porting").html("")
+                            fillFilterItem(Array.from(porting).sort(), $('.row.wheels .filter__item-list.porting'), 'porting')
+                        }
+                        if (!inputName.has('diameter')) {
+                            $(".filter__item-list.diameter").html("")
+                            fillFilterItem(Array.from(diameter).sort(), $('.row.wheels .filter__item-list.diameter'), 'diameter')
+                        }
+                        if (!inputName.has('width')) {
+                            $(".filter__item-list.width").html("")
+                            fillFilterItem(Array.from(width).sort(), $('.row.wheels .filter__item-list.width'), 'width')
+                        }
+                        if (!inputName.has('gab')) {
+                            $(".filter__item-list.gab").html("")
+                            fillFilterItem(Array.from(gab).sort(), $('.row.wheels .filter__item-list.gab'), 'gab')
+                        }
+                        if (!inputName.has('dia')) {
+                            $(".filter__item-list.dia").html("")
+                            fillFilterItem(Array.from(dia).sort(), $('.row.wheels .filter__item-list.dia'), 'dia')
+                        }
+                        if (!inputName.has('brand')) {
+                            $(".filter__item-list.brand").html("")
+                            fillFilterItem(Array.from(brand).sort(), $('.row.wheels .filter__item-list.brand'), 'brand')
+                        }
+
+                        $('.filter__loading').css('display', 'none')
+                        $('.catalog__loading').css('display', 'none')
+
+                        setTimeout(() => {
+                            selector.off("click")
+                            selector = $('.filter .row:not(.cars) input[type="radio"]:not(:checked)+label, .filter .row:not(.cars) input[type="checkbox"]+label')
+                            selector.on("click", func(selector, inputName, catalogBlock, filterType))
+                        }, 1500)
+
+                        let parameters = "/"
+
+                        for (let input in inputs) {
+                            if (inputs[input].length) {
+                                if (inputs[input].length > 1) {
+                                    parameters += `${input}-is-`
+                                }
+                                inputs[input].forEach(i => {
+                                    if (inputs[input].length > 1) {
+                                        parameters += `${i}-or-`
+                                    }
+                                    else {
+                                        parameters += `${input}-is-${i}`
+                                    }
+                                })
+                                if (inputs[input].length > 1) {
+                                    parameters = parameters.substring(0, parameters.length - 4)
+                                }
+                                parameters += '/'
+                            }
+                        }
+
+                        parameters = parameters.toLowerCase()
+
+                        let text;
+
+                        if (filterType != 'wheels') {
+                            switch (inputs['type'][0]) {
+                                default:
+                                    text = 'Легковые авто'
+                                    break
+                                case '2':
+                                    text = 'Грузовые авто'
+                                    break
+                                case '3':
+                                    text = 'Мото-квадро'
+                                    break
+                            }
+                        }
+
+                        history.replaceState(null, null,  `/sibirkoleso/${filterType}.html` + parameters)
+                        filterTitle.html(
+                            text +
+                            "<img alt=\"\" class=\"filter__type-img\" src=\"/img/arrow.svg\">"
+                        )
+                        if(filterTitle.text() != 'Легковые авто') {
+                            filterSeasons.find(".row").removeClass('active')
+                            filterSeasons.css('display', 'none')
+                        } else {
+                            filterSeasons.find(".row").addClass('active')
+                            $('.filter__type-list input').removeAttr('checked')
+                            filterSeasons.css('display', 'block')
+                            $('.filter__type-list').removeClass('open')
+                        }
+                    }
+                })
+            }
+            else {
+                $.ajax({
+                    url: '/data.json',
+                    success: (data) => {
+                        let width = new Set()
+                        let height = new Set()
+                        let diameter = new Set()
+                        let brand = new Set()
+                        let empty = true
+                        let match = true
+
+                        for (let input in inputs) {
+                            inputs[input].forEach(() => {
+                                empty = false
+                            })
+                            if (!empty) {
+                                break
+                            }
+                        }
+
+                        if (empty) {
+                            $('.filter__loading').css('display', 'none')
+                            return data;
+                        }
+                        catalogBlock.html("")
+
+                        data.forEach(d => {
+                            for (let input in inputs) {
+                                if (match) {
+                                    for (let val in inputs[input]) {
+                                        if (d[input] == inputs[input][val]) {
+                                            match = true;
+                                            width.add(d.width)
+                                            height.add(d.height)
+                                            diameter.add(d.diameter)
+                                            brand.add(d.brand)
+                                            break;
+                                        }
+                                        match = false
+                                    }
+                                }
+                            }
+                            if (match) {
+                                fillCatalog(d)
+                            }
+                            match = true
+                        })
+
+
+                        inputName.add($(this).prev().attr("name"))
+
+                        if (!inputName.has('width')) {
+                            $(".filter__item-list.width").html("")
+                            fillFilterItem(Array.from(width).sort(), $('.row.tyres .filter__item-list.width'), 'width')
+                        }
+                        if (!inputName.has('height')) {
+                            $(".filter__item-list.height").html("")
+                            fillFilterItem(Array.from(height).sort(), $('.row.tyres .filter__item-list.height'), 'height')
+                        }
+                        if (!inputName.has('diameter')) {
+                            $(".filter__item-list.diameter").html("")
+                            fillFilterItem(Array.from(diameter).sort(), $('.row.tyres .filter__item-list.diameter'), 'diameter')
+                        }
+                        if (!inputName.has('brand')) {
+                            $(".filter__item-list.brand").html("")
+                            fillFilterItem(Array.from(brand).sort(), $('.row.tyres .filter__item-list.brand'), 'brand')
+                        }
+
+                        $('.filter__loading').css('display', 'none')
+                        $('.catalog__loading').css('display', 'none')
+
+                        setTimeout(() => {
+                            selector.off("click")
+                            selector = $('.filter .row:not(.cars) input[type="radio"]:not(:checked)+label, .filter .row:not(.cars) input[type="checkbox"]+label')
+                            selector.on("click", func(selector, inputName, catalogBlock, filterType))
+                        }, 1500)
+
+                        let parameters = "/"
+
+                        for (let input in inputs) {
+                            if (inputs[input].length) {
+                                if (inputs[input].length > 1) {
+                                    parameters += `${input}-is-`
+                                }
+                                inputs[input].forEach(i => {
+                                    if (inputs[input].length > 1) {
+                                        parameters += `${i}-or-`
+                                    } else {
+                                        parameters += `${input}-is-${i}`
+                                    }
+                                })
+                                if (inputs[input].length > 1) {
+                                    parameters = parameters.substring(0, parameters.length - 4)
+                                }
+                                parameters += '/'
+                            }
+                        }
+
+                        parameters = parameters.toLowerCase()
+
+                        let text;
+
+                        switch (inputs['type'][0]) {
+                            default:
+                                text = 'Легковые авто'
+                                break
+                            case '2':
+                                text = 'Грузовые авто'
+                                break
+                            case '3':
+                                text = 'Мото-квадро'
+                                break
+                        }
+
+                        history.replaceState(null, null, `/sibirkoleso/${filterType}.html` + parameters)
+                        filterTitle.html(
+                            text +
+                            "<img alt=\"\" class=\"filter__type-img\" src=\"/img/arrow.svg\">"
+                        )
+                        if (filterTitle.text() != 'Легковые авто') {
+                            filterSeasons.find(".row").removeClass('active')
+                            filterSeasons.css('display', 'none')
+                        } else {
+                            filterSeasons.find(".row").addClass('active')
+                            $('.filter__type-list input').removeAttr('checked')
+                            filterSeasons.css('display', 'block')
+                            $('.filter__type-list').removeClass('open')
+                        }
+                    }
+                })
+            }
+
+        }, 0)
+    })
+}
---
+++ js/main.js
@@ -0,0 +1,175 @@
+
+setTimeout(() => {
+$(document).ready(function() {
+    let filterTab = $('.filter__tab')
+    let filterSeasons = $('.filter__seasons')
+
+    filterTab.on('click', function (e) {
+        e.preventDefault()
+        if (!$(this).hasClass('filter__tab_active')) {
+            filterTab.removeClass('filter__tab_active')
+            $(this).addClass('filter__tab_active')
+            $('.filter__tab-cursor').toggleClass('filter__tab-cursor_wheels')
+            $('.filter__tab-cursor').toggleClass('filter__tab-cursor_tyres')
+
+            $('.filter__sub-tab').removeClass('filter__sub-tab_active')
+            $(`.filter__sub-tab.params`).addClass('filter__sub-tab_active')
+            $('.filter__body .row').removeClass('active')
+            if ($(this).text() == 'Диски') {
+                filterType = 'wheels'
+                $('.filter__body .row.wheels').addClass('active')
+            }
+            else {
+                filterType = 'tyres'
+                $('.filter__body .row.tyres').addClass('active')
+            }
+            history.replaceState(null, null,  `/sibirkoleso/${filterType}.html`)
+            window.location.reload()
+        }
+    })
+
+    let filterSubTab = $('.filter__sub-tab')
+    let sort = $('.sort')
+
+    filterSubTab.on('click', function () {
+        filterSubTab.removeClass('filter__sub-tab_active')
+        $(this).toggleClass('filter__sub-tab_active')
+
+        let type = $(this).attr("class").split(/\s+/)[1]
+
+
+        $('.row').removeClass('active')
+        if (type == 'cars') {
+            $('.row.' + type).addClass('active')
+            sort.css('display', 'block')
+        } else {
+            $('.row.' + type + '.' + filterType).addClass('active')
+            sort.css('display', 'none')
+        }
+    })
+
+   var filterTitle = $('.filter__type-title')
+
+    filterTitle.on('click', function () {
+        $('.filter__type-list').toggleClass('open')
+    })
+
+    $('.filter__type-list label').on('click', function() {
+        filterTitle.html(
+            $(this).text() +
+            "<img alt=\"\" class=\"filter__type-img\" src=\"/img/arrow.svg\">"
+        )
+        if(filterTitle.text() != 'Легковые авто') {
+            filterSeasons.find(".row").removeClass('active')
+            filterSeasons.css('display', 'none')
+        } else {
+            filterSeasons.find(".row").addClass('active')
+            $('.filter__type-list input').removeAttr('checked')
+            filterSeasons.css('display', 'block')
+            $('.filter__type-list').removeClass('open')
+        }
+    })
+
+    let thorn = $('.thorn')
+
+    $('.filter__seasons-item').on('click', function () {
+        if ($(this).hasClass('winter') || $('.winter input').is(':checked')) {
+            thorn.css('display', 'inline-block')
+        } else {
+            thorn.css('display', 'none')
+            thorn.find('input').prop('checked', false)
+        }
+    })
+
+    $('.filter__item-title').on('click', function () {
+        $(this).parents(".filter__item").toggleClass('activated')
+    })
+
+    for (let i = 1; i < 4; i++) {
+        $(`.cars-${i} label`).on('click', function () {
+            $(`.cars-${i+1}`).removeClass('disabled')
+        })
+    }
+
+    let catalogSortItem = $('.catalog__sort-item a')
+
+    catalogSortItem.on('click', function (e) {
+        e.preventDefault()
+
+        if ($(this).hasClass('active')) {
+            let catalogIcon = $('.catalog__icon')
+            catalogIcon.toggleClass("desc")
+            catalogIcon.toggleClass("asc")
+
+            let order = catalogIcon.attr("class").split(/\s+/)[1]
+
+            // $.ajax({
+            //     url: "data.json",
+            //     data: {"order" : order}
+            //     success: (data) => {
+            //
+            //      }
+            // })
+
+            switch (order) {
+                case 'asc':
+                    catalogIcon.html('<path d="M173,169v-1h4v1h-4Zm-4-5h8v1h-8v-1Zm-4-4h12v1H165v-1Z" transform="translate(-165 -160)"/>')
+                    break
+                case 'desc':
+                    catalogIcon.html('<path d="M190,169v-1h12v1H190Zm4-5h8v1h-8v-1Zm4-4h4v1h-4v-1Z" transform="translate(-190 -160)"/>')
+                    break
+            }
+        } else {
+            catalogSortItem.removeClass('active')
+            $(this).addClass('active')
+        }
+    })
+
+    let faderSideBar = $('.fader_sidebar')
+    let faderMenu = $('.fader_menu')
+    let body = $('body')
+    let sideBar = $('.sidebar')
+    let sideBarSub = $('.sidebar__sub')
+    let menuPopUp = $('.main-menu-popup')
+
+    $('.header__cart').on('click', function () {
+        body.toggleClass('noscroll')
+        faderSideBar.toggleClass('show')
+        sideBar.toggleClass('open')
+    })
+
+    $('.wild').on('click', function () {
+        sideBarSub.toggleClass('open')
+    })
+
+    sideBarSub.find('.close').on('click', function () {
+        sideBarSub.toggleClass('open')
+    })
+
+    $('.cart__close').on('click', function () {
+        body.toggleClass('noscroll')
+        faderSideBar.toggleClass('show')
+        sideBar.toggleClass('open')
+    })
+
+    $('.header__menu-button').on('click', function () {
+        menuPopUp.toggleClass('open')
+        faderMenu.toggleClass('show')
+        $(this).toggleClass('open')
+    })
+
+    faderSideBar.on('click', function () {
+        body.removeClass('noscroll')
+        sideBar.removeClass('open')
+        sideBarSub.removeClass('open')
+        $(this).removeClass('show')
+    })
+
+    faderMenu.on('click', function () {
+        menuPopUp.removeClass('open')
+        $('.header__menu-button').removeClass('open')
+        $(this).removeClass('show')
+    })
+})
+}, 100)
+
